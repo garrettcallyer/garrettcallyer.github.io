07d7f68e3c5900b4b8b50019a8feac5231f0f65a
garrettcallyer <89547101+garrettcallyer@users.noreply.github.com>
2021-09-09 18:14:35 -0500

    New pictures and updates to
    New images and added functionality

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
-  $('#jokeFact').click(function() {
+  $('#jokeFact1').click(function() {
     $.ajax({
       dataType: "json",
       url: "http://api.icndb.com/jokes/random.json",
-      success: function(getJoke) {
-        console.log(getJoke["value"]["joke"]);
-        let str = getJoke["value"]["joke"]
-        if str.includes("")
-        $('p').text(getJoke["value"]["joke"]);
+      success: function(getJoke1) {
+        console.log(getJoke1["value"]["joke"]);
+          $('#Joke1').text(getJoke1["value"]["joke"]);
+        },
+        error: function(xhr,status,error) {
+        console.log(error);
+      }
+      })
+    });
+  });
+
+$(document).ready(function() {
+  $('#jokeFact2').click(function() {
+    $.ajax({
+      dataType: "json",
+      url: "https://api.chucknorris.io/jokes/random",
+      success: function(getJoke2) {
+        console.log(getJoke2["value"]);
+          $('#Joke2').text(getJoke2["value"]);
       },
       error: function(xhr,status,error) {
-        console.log(error);
+      console.log(error);
       }
     });
   });
